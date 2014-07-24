7fa60e716674869f141d65b26797df504fa25441
ssworks <twistiesbros@gmail.com>
2014-07-24 11:25:54 -0700

    first push

---
+++ Desktop/Google Drive/WORK/CLIENTS/NFC/meeting/wp-content/themes/JointsWP-CSS-master/partials/nav-offcanvas.php
@@ -0,0 +1,22 @@
+
+<div class="large-12 columns">
+	<div class="contain-to-grid">
+		<nav class="tab-bar" data-topbar>
+			<section class="left tab-bar-section">
+				<!-- <h1 class="title"><img src="http://inside.geometrysf.com/wp-admin/images/gg_logo.png"></h1> -->
+			</section>
+			<section class="left-small">
+				<a class="left-off-canvas-toggle menu-icon" ><span>GET STARTED</span></a>
+			</section>
+		</nav>
+	</div>
+</div>
+						
+<aside class="left-off-canvas-menu">
+	<ul class="off-canvas-list">
+		<li><label>Navigation</label></li>
+			<?php joints_main_nav(); ?>    
+	</ul>
+</aside>
+			
+<a class="exit-off-canvas"></a>
